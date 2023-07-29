ee61d16f3eace4267a82363a906d97e6d6b2631f
palijugran <pali.jugran@quokkalabs.com>
2023-07-29 06:30:42 +0530

    fetch group with search text

--- src/app/routes/admin.js
+++ src/app/routes/admin.js
@@ -6,7 +6,7 @@ const isAuth = require("../middlewares/isAuth");
 
 router.post("/login", authController.login);
 router.post("/create-update-group", isAuth, GroupController.createUpdateGroup);
-router.post("/fetch-group", isAuth, GroupController.fetchGroupsByStatus);
+router.post("/fetch-group", isAuth, GroupController.fetchGroup);
 router.post("/approve-group-request", isAuth, GroupController.approveRequest);
 router.post("/enable-disable-group", isAuth, GroupController.disableStatus);
 
